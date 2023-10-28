a42ac7f2e5ae855209c6b8f867768e48d0a625f6
Luis <luisedgucv@gmail.com>
2023-10-28 12:25:51 -0500

    Agregando diferentes labels

--- intro.py
+++ intro.py
@@ -2,11 +2,16 @@ from tkinter import *
 
 root = Tk()
 root.title('Hola mundo')
-root.geometry('400x400')
+root.geometry('500x500')
 
-label = Label(root, text='Hola Mundo! mi primera etiqueta')
-# Label(root, text='Hola Mundo! mi primera etiqueta').pack
+l1 = Label(root, text='Hola Mundo! primera etiqueta')
+l2 = Label(root, text='Chao Mundo! segunda etiqueta')
+l3 = Label(root, text='         ')
 
-label.pack()
+# Label(root, text='Hola Mundo! mi primera etiqueta').pack()
+
+l1.grid(row=0, column=0)
+l3.grid(row=1, column=1)
+l2.grid(row=10, column=10)
 
 root.mainloop()
